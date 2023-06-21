8c24628b6478582cbe4729b744ac8643543ab129
Jack Bucher <jabucher@181015-B-APL-PV.local>
2023-06-21 19:27:15 +1000

    Start day 9

--- advent-of-code-2022/day09/src/main.rs
+++ advent-of-code-2022/day09/src/main.rs
@@ -1,3 +1,47 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+
+struct Postition {
+    row: i32,
+    col: i32 
+}
+
+
+#[derive(Debug)]
+enum Move {
+    Vertical(i32),
+    Horizontal(i32),
+}
+
+impl Move {
+    fn from_line(line: String) -> Move {
+        let movement = line.split_once(" ").expect("Can't split line");
+        let dist: i32 = movement.1.parse().expect("Invalid distance");
+        match movement.0 {
+            "U" => Move::Vertical(dist),
+            "D" => Move::Vertical(-dist),
+            "R" => Move::Horizontal(dist),
+            "L" => Move::Horizontal(-dist),
+            _ => panic!("Unhandled Instruction"),
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "example.txt";
+    // let filename = "puzzleInput.txt";
+
+    let head_pos = Postition {row: 0, col: 0};
+    let tail_pos = Postition {row: 0, col: 0};
+
+    let reader = BufReader::new(File::open(filename)
+        .expect("Cannot open file"));
+
+    for line in reader.lines() {
+        let movement = Move::from_line(line.expect("Could not get line"));
+        println!("{:?}", movement);
+    }
+
 }
