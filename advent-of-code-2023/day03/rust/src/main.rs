21daf7b2509ca36685b4515c86d442d7e7f33d5e
jamibu <bucher.codes@gmail.com>
2023-12-04 22:26:12 +1100

    Change day 3 to search for symbols not numbers

--- advent-of-code-2023/day03/rust/src/main.rs
+++ advent-of-code-2023/day03/rust/src/main.rs
@@ -2,12 +2,6 @@ use std::fs;
 use std::error::Error;
 
 
-struct Coord {
-    x: usize,
-    y: usize,
-}
-
-
 fn main() -> Result<(), Box<dyn Error>> {
     let fname = "../example.txt";
     let input_str: String = fs::read_to_string(fname)?;
@@ -23,41 +17,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part1(rows: &Vec<Vec<char>>) {
-    for (y, row) in rows.iter().enumerate() {
-        let mut x = 0;
-        while x < row.len() {
-            if row[x].is_numeric() || row[x] == '0'{
-                x += 1;
-                continue;
-            }
+    let visited = Vec<Vec<bool>> = vec!()
 
-            let last_x = row[x..]
-                .iter()
-                .position(|&r| !r.is_numeric())
-                .unwrap() + x - 1;
-
-            // check_surrounding(rows, x, end_x, y);
-            // Go to next char after the number
-            x += last_x + 1;
+    for (i, row) in rows.iter().enumerate() {
+        for (j, char) in row.chars().enumerate() {
         }
     }
 }
 
-fn get_surrounding(first_x: usize, last_x: usize, y: usize, rows: usize, cols: usize) {
-    if first_x == 0 {
-
-    }
-
-    if last_x == 
-
-    let start_x = first_x - 1 * (first_x > 0) as usize;
-    let end_x = last_x + 1 * (last_x > 0) as usize;
-    
-    let mut surrounding: Vec<usize> = vec![];
-
-    if y > 0 {
-        let y_top = y - 1;
-        surrounding.append((start_x..end_x).collect());
-    }
-
-}
