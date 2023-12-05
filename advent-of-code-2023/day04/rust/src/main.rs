ce0148ec6e625b213e6b0e2ec958041607abe55c
Jack <jack.bucher@orygen.org.au>
2023-12-05 19:20:03 +1100

    Cleanup for day04.

--- advent-of-code-2023/day04/rust/src/main.rs
+++ advent-of-code-2023/day04/rust/src/main.rs
@@ -13,9 +13,7 @@ fn main() {
     let mut part2 = 0;
     let mut cards: [usize; NUM_CARDS] = [1; NUM_CARDS];
     for (i, line) in lines.enumerate() {
-        println!("{:?}", line);
         let wins: usize = parse_card(line.unwrap());
-        println!("{}", wins);
         if wins > 0 {
             part1 += 2u32.pow(wins as u32 - 1)
         }
@@ -41,7 +39,6 @@ fn parse_card(line: String) -> usize {
 }
 
 fn process_card(cards: &mut [usize; NUM_CARDS], wins: usize, idx: usize) -> usize {
-    println!("{} {}", idx, wins);
     for j in (idx + 1)..=(idx + wins) {
         cards[j] += cards[idx]
     }
