c276e3fd31e32108c7e84cb93da14c5427d10fd2
Jack <jack.bucher@orygen.org.au>
2023-12-05 18:08:42 +1100

    Day04 solved.

--- advent-of-code-2023/day04/rust/src/main.rs
+++ advent-of-code-2023/day04/rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -6,23 +6,39 @@ fn main() {
     let fname = "../puzzleInput.txt";
     let file = File::open(fname).expect("Could not open file");
     let lines = io::BufReader::new(file).lines();
-    let part1: u32 = lines.map(|x| parse_card(x.unwrap())).sum();
-    println!("Part 1: {}", part1);
+    let matches: Vec<usize> = lines.map(|x| parse_card(x.unwrap())).collect();
+
+    let part_1: u32 = matches
+        .iter()
+        .filter(|x| **x > 0)
+        .fold(0u32, |a, x| a + 2u32.pow(*x as u32 - 1));
+    println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part2(&matches));
 }
 
-fn parse_card(line: String) -> u32 {
+fn parse_card(line: String) -> usize {
     let card: Vec<&str> = line.split(':').last().unwrap().split('|').collect();
     let winners: HashSet<u32> = card[0]
         .split_whitespace()
         .map(|x| x.parse::<u32>().unwrap())
         .collect();
-    let num_match: usize = card[1]
+    return card[1]
         .split_whitespace()
         .map(|x| x.parse::<u32>().unwrap())
         .filter(|x| winners.contains(x))
         .count();
-    if num_match == 0 {
-        return 0;
+}
+
+fn part2(matches: &Vec<usize>) -> usize {
+    let mut cards: VecDeque<usize> = VecDeque::from((0..matches.len()).collect::<Vec<usize>>());
+    let mut total_cards: usize = 0;
+    while !cards.is_empty() {
+        let card_idx = cards.pop_front().unwrap();
+        for idx in 1..=matches[card_idx] {
+            cards.push_back(card_idx + idx)
+        }
+        total_cards += 1
     }
-    return 2u32.pow(num_match as u32 - 1);
+
+    return total_cards;
 }
